f9ed800c9bf594d52337a3ecc686ad558941a120
Finn von Heesen <finnvhsn@gmail.com>
2025-03-25 15:19:38 +0100

    hash eliminate

--- evaluate.py
+++ evaluate.py
@@ -2,16 +2,16 @@
 
 def evaluate_model_output(prompt, model_output, test_code):
     try:
-        # Codestring erstellen
+ 
         full_code = prompt + "\n" + model_output + "\n" + test_code
 
-        # definiere eigenen Ausführungsraum
+    
         local_ns = {}
 
-        # führe Code aus
+      
         exec(full_code, {}, local_ns)
 
-        # check() aufrufen mit generierter Funktion
+      
         func_name = extract_function_name(prompt)
         candidate_func = local_ns.get(func_name)
         
@@ -31,19 +31,19 @@ def extract_function_name(prompt: str) -> str:
     for line in prompt.splitlines():
         line = line.strip()
         if line.startswith("def "):
-            name_part = line.split("(")[0]  # z. B. "def truncate_number"
+            name_part = line.split("(")[0]  
             return name_part.replace("def ", "").strip()
-    return None  # fallback
+    return None  
 
     
 import sqlite3
 import pandas as pd
 
-# Datenbankverbindung
+
 conn = sqlite3.connect("humaneval.db")
 df = pd.read_sql("SELECT * FROM results", conn)
 
-# Bewertung starten
+
 results = []
 
 for _, row in df.iterrows():
@@ -54,6 +54,5 @@ for _, row in df.iterrows():
     )
     results.append(result)
 
-# Test-Resultate in neuer Tabelle speichern
 df["test_result"] = results
 df.to_sql("results_evaluated", conn, if_exists="replace", index=False)
